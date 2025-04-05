0d63d4c6287a799fbd5b6014abac530129455a69
Misael Lima <misaellimadossantos@hotmail.com>
2025-04-05 18:58:22 -0400

    feat: add global styles

---
+++ src/styles/index.ts
@@ -0,0 +1,15 @@
+export const BLUE_DARK = '#1E6F9F';
+export const BLUE = '#4EA8DE';
+export const PURPLE_DARK = '#5E60CE';
+export const PURPLE = '#8284FA';
+
+export const GRAY_100 = '#F2F2F2';
+export const GRAY_200 = '#D9D9D9';
+export const GRAY_300 = '#808080';
+export const GRAY_400 = '#333333';
+export const GRAY_500 = '#262626';
+export const GRAY_600 = '#1A1A1A';
+export const GRAY_700 = '#0D0D0D';
+
+export const DANGER = '#E25858';
+
